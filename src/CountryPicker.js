b7328fe573b27b5f900933acf2111049d7ffc760
Danny Shisler <danny@shisler.com>
2017-12-28 23:25:44 +0000

    Fix React 16 PropTypes

--- src/CountryPicker.js
+++ src/CountryPicker.js
@@ -20,6 +20,7 @@ import {
   Platform,
 } from 'react-native';
 import Fuse from 'fuse.js';
+import PropTypes from 'prop-types';
 
 import cca2List from '../data/cca2';
 import { getHeightPercent } from './ratio';
@@ -52,18 +53,18 @@ const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
 export default class CountryPicker extends Component {
   static propTypes = {
-    cca2: React.PropTypes.string.isRequired,
-    translation: React.PropTypes.string,
-    onChange: React.PropTypes.func.isRequired,
-    onClose: React.PropTypes.func,
-    closeable: React.PropTypes.bool,
-    filterable: React.PropTypes.bool,
-    children: React.PropTypes.node,
-    countryList: React.PropTypes.array,
-    excludeCountries: React.PropTypes.array,
-    styles: React.PropTypes.object,
-    filterPlaceholder: React.PropTypes.string,
-    autoFocusFilter: React.PropTypes.bool,
+    cca2: PropTypes.string.isRequired,
+    translation: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    onClose: PropTypes.func,
+    closeable: PropTypes.bool,
+    filterable: PropTypes.bool,
+    children: PropTypes.node,
+    countryList: PropTypes.array,
+    excludeCountries: PropTypes.array,
+    styles: PropTypes.object,
+    filterPlaceholder: PropTypes.string,
+    autoFocusFilter: PropTypes.bool,
   }
 
   static defaultProps = {
